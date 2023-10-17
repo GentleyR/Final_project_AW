71e33d269d398e77eccbcc77a5d97fb69a8b0e10
NkoLow <104155407+NkoLow@users.noreply.github.com>
2023-10-17 16:51:24 -0700

    Revert "express"
    This reverts commit c4e9bd17086206dc04776d50d53de3c092a0e7df.

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
-const port = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/api/data/:arg', (req, res) => {
-    const arg = req.params.arg;
-    const result = generateResponse(arg);
-    res.json(result);
+// Test route
+app.get('/', (req, res) => {
+    res.send('Movie Review API is running!');
 });
 
-function generateResponse(arg) {
-    return { message: `This is the button ${arg}` };
-}
-
-app.all('*', (req, res) =>
-    res.status(404)
-        .send('<h1>ERROR 404! Page not found!</h1>')
-);
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
